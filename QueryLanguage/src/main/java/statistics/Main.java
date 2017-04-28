caee40f31343a9d1fb2ed5d79b473d87133797bd
Owlaukka <olli.laukkanen3@gmail.com>
2017-04-28 17:07:02 +0300

    tehtava 4 teh tehty

--- QueryLanguage/src/main/java/statistics/Main.java
+++ QueryLanguage/src/main/java/statistics/Main.java
@@ -7,8 +7,8 @@ public class Main {
         Statistics stats = new Statistics(new PlayerReaderImpl("http://nhlstats-2013-14.herokuapp.com/players.txt"));
           
         Matcher m = new And( new HasAtLeast(10, "goals"),
-                             new HasAtLeast(10, "assists"),
-                             new PlaysIn("PHI")
+                             new HasFewerThan(20, "assists"),
+                             new Not(new PlaysIn("PHI"))
         );
         
         for (Player player : stats.matches(m)) {
